1f05e9d0776644fed0c7cf6405304bdb7d6a2ab1
Lopamudra1527 <62988095+Lopamudra1527@users.noreply.github.com>
2021-08-11 18:42:06 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-dark">
+    <a class="navbar-brand" href="index.php" style="color:#ECFAFE;">The Sparks Foundation Project</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+             <a class="nav-link" href="index.php" style="color: #ECFAFE ;">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="viewallcustomers.php" style="color: #ECFAFE ;">View Users</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color: #ECFAFE ;">Transfer Money</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color: #ECFAFE ;">Transaction History</a>
+           </li>
+    </div>
+</nav>
